fe6242b7a4900a728d3c1a8c5072d69d0b1b26d6
Spencer Thornock <spencer.thornock@gmail.com>
2013-08-07 14:54:13 -0700

    testing out angular seed

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,16 @@
-'use strict';
-
-
-// Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ajoslin.mobile-navigate','ngMobile'])
-    .config(function ($compileProvider){
+angular.module('App', ['App.services', 'App.controllers'])
+    .config(['$compileProvider', function ($compileProvider) {
         $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-    })
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
-        $routeProvider.when('/view1', {templateUrl: 'partials/notificationView.html'});
-        $routeProvider.when('/view2', {templateUrl: 'partials/geolocationView.html'});
-        $routeProvider.when('/view3', {templateUrl: 'partials/accelerometerView.html'});
-        $routeProvider.when('/view4', {templateUrl: 'partials/deviceInfoView.html'});
-        $routeProvider.when('/view5', {templateUrl: 'partials/cameraView.html'});
-        $routeProvider.when('/view6', {templateUrl: 'partials/contactsView.html'});
-        $routeProvider.when('/view7', {templateUrl: 'partials/compassView.html'});
-        $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+    }])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller: 'MainCtrl',
+            templateUrl: 'partials/main.html'
+        })
+        .when('/view', {
+            controller: 'ViewCtrl',
+            templateUrl: 'partials/view.html'
+        })
+        .otherwise({redirectTo: '/'});
+    }]);
